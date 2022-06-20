655e018e21698eb4bc32c1b8f620a263990833a9
Patricia Padilha <patriciapadilhabio@gmail.com>
2022-06-20 18:37:59 -0300

    Requisito 2 - pesquisar por id na rota /talker/:id

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const PORT = '3000';
 
 app.get('/talker', services.getTalkers);
 
+app.get('/talker/:id', services.getTalkerById);
+
 // não remova esse endpoint, para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
